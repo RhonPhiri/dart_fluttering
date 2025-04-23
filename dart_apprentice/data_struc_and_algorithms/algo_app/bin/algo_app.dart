990afb11c082b609a420c084ffeb881d6f506ea3
RhonPhiri <166160995+RhonPhiri@users.noreply.github.com>
2025-04-23 14:51:32 +0200

    Started practicing linked lists

--- dart_apprentice/data_struc_and_algorithms/algo_app/bin/algo_app.dart
+++ dart_apprentice/data_struc_and_algorithms/algo_app/bin/algo_app.dart
@@ -1,6 +1,9 @@
 import 'stack/stack.dart';
+import 'linked_lists/linked_list.dart';
 
 void main() {
   //method to run all the things in the stack page
-  runStack();
+  // runStack();
+  //method to run all contents in the linked list folder
+  runLinkedList();
 }
